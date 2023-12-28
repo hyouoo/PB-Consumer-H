4e1509fc39e45c1a2a595f5526e75f9fef2fcfc7
Joonyoung Hong <hjunyoung10@gmail.com>
2023-12-28 23:54:18 +0900

    Feat: Event 관련 consumer config 설정

---
+++ src/main/java/com/example/purebasketconsumer/config/KafkaEventConsumerConfig.java
@@ -0,0 +1,45 @@
+package com.example.purebasketconsumer.config;
+
+import com.example.purebasketconsumer.consumer.dto.KafkaEventDto;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaEventConsumerConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootStrapServers;
+
+    @Value("${spring.kafka.consumer.group-id.event}")
+    private String groupId;
+
+    @Bean
+    public ConsumerFactory<String, KafkaEventDto> eventConsumerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServers);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(),
+                new JsonDeserializer<>(KafkaEventDto.class, false)
+        );
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, KafkaEventDto> kafkaEventListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, KafkaEventDto> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(eventConsumerFactory());
+
+        return factory;
+    }
+}
