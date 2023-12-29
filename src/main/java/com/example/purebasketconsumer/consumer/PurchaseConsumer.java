fac4f39ea21c93d928283f23296be6aed0e921c9
Joonyoung Hong <hjunyoung10@gmail.com>
2023-12-29 21:27:10 +0900

    Update: Batch Insert/Delete로 PurchaseConsumer 최적화
    여러 상품을 한 번에 구매할 때 쿼리가 여러 개를 생성하지 않고, 하나의 쿼리만 생성되도록 최적화

--- src/main/java/com/example/purebasketconsumer/consumer/PurchaseConsumer.java
+++ src/main/java/com/example/purebasketconsumer/consumer/PurchaseConsumer.java
@@ -1,15 +1,15 @@
 package com.example.purebasketconsumer.consumer;
 
-import com.example.purebasketconsumer.domain.cart.CartRepository;
+import com.example.purebasketconsumer.consumer.dto.KafkaPurchaseDto;
 import com.example.purebasketconsumer.domain.member.entity.Member;
 import com.example.purebasketconsumer.domain.product.ProductRepository;
 import com.example.purebasketconsumer.domain.product.entity.Product;
-import com.example.purebasketconsumer.domain.purchase.PurchaseRepository;
-import com.example.purebasketconsumer.consumer.dto.KafkaPurchaseDto;
 import com.example.purebasketconsumer.domain.purchase.dto.PurchaseRequestDto.PurchaseDetail;
 import com.example.purebasketconsumer.domain.purchase.entity.Purchase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -17,6 +17,8 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,9 +26,8 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class PurchaseConsumer {
-    private final PurchaseRepository purchaseRepository;
     private final ProductRepository productRepository;
-    private final CartRepository cartRepository;
+    private final JdbcTemplate jdbcTemplate;
 
     @KafkaListener(topics = "${spring.kafka.consumer.topics.purchase}", groupId = "${spring.kafka.consumer.group-id.purchase}",
             containerFactory = "kafkaPurchaseListenerContainerFactory" ,concurrency = "3")
@@ -36,9 +37,7 @@ public class PurchaseConsumer {
                                  @Header(KafkaHeaders.RECEIVED_PARTITION) List<Integer> partitions,
                                  @Header(KafkaHeaders.RECEIVED_TOPIC) String topics
                                  ) {
-        System.out.println("offsets = " + offsets);
-        System.out.println("partitions = " + partitions);
-        System.out.println("topics = " + topics);
+        log.info("topics: {}, partitions: {}, offsets: {}", topics, partitions, offsets);
 
         List<PurchaseDetail> purchaseDetails = data.purchaseRequestDto();
         Member member = data.member();
@@ -55,9 +54,49 @@ public class PurchaseConsumer {
             purchaseList.add(purchase);
         }
 
-        purchaseRepository.saveAll(purchaseList);
-        cartRepository.deleteByMemberAndProductIdIn(member, requestedProductsIds);
+        purchaseBatchInsert(purchaseList);
+        cartBatchDelete(member, requestedProductsIds);
         log.info("회원 {}: 상품 구매 완료 - {}", member.getId(), purchaseDetails);
+    }
+
+    public void purchaseBatchInsert(List<Purchase> purchaseList) {
+        String sql = "INSERT INTO purchase "
+                + "(amount, price, member_id, product_id, purchased_at) VALUE (?, ?, ?, ?, ?)";
+
+        jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement ps, int i) throws SQLException {
+                        Purchase purchase = purchaseList.get(i);
+                        ps.setInt(1, purchase.getAmount());
+                        ps.setInt(2, purchase.getPrice());
+                        ps.setLong(3, purchase.getMember().getId());
+                        ps.setLong(4, purchase.getProduct().getId());
+                        ps.setTimestamp(5, java.sql.Timestamp.valueOf(java.time.LocalDateTime.now()));
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return purchaseList.size();
+                    }
+                }
+        );
+    }
+
+    public void cartBatchDelete(Member member, List<Long> requestedProductsIds) {
+        String sql = "DELETE FROM cart WHERE member_id = ? AND product_id IN (?)";
+
+        jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                ps.setLong(1, member.getId());
+                ps.setLong(2, requestedProductsIds.get(i));
+            }
+
+            @Override
+            public int getBatchSize() {
+                return requestedProductsIds.size();
+            }
+        });
     }
 
 }
